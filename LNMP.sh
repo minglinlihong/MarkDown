118a87fee98eb40558133645d4cd415fc3c4ee8a
minglinlihong <minglinlihong@163.com>
2019-08-08 16:35:37 +0800

    Update LNMP.sh

--- LNMP.sh
+++ LNMP.sh
@@ -1,52 +1,15 @@
 #！/usr/bin/env bash
-#安装jdk,tomcat,mysql,nginx,php
+#mysql,nginx,php
 
 ##下载软件
 set -x
 cd /tmp
-wget -c --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-https://download.oracle.com/otn/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz?AuthParam=1565060938_11f832ef80bc0a00eac67cef7ad42655
-
-wget -c http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz
-wget -c http://nginx.org/download/nginx-1.12.2.tar.gz
 wget -c http://mirrors.sohu.com/mysql/MySQL-5.7/mysql-5.7.26-el7-x86_64.tar.gz ##使用国内镜像源会快一点点
+wget -c http://nginx.org/download/nginx-1.12.2.tar.gz
 wget -c https://www.php.net/distributions/php-7.2.21.tar.gz
-
 set +x
 
 yum  -y groupinstall  "Development Tools"   ##安装开发工具，编译时会用到
-##安装jdk
-[ -f /tmp/jdk-8u101-linux-x64.tar.gz ] && tar -xf /tmp/jdk-8u101-linux-x64.tar.gz -C /usr/local
-cat >> /etc/profile <<-EOF
-##jdk
-export JAVA_HOME=/usr/local/jdk1.8.0_101
-export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
-export PATH=$JAVA_HOME/bin:$PATH
-EOF
-source /etc/profile
-java -version > /dev/null
-retval=$?
-if [[ $retval -ne 0 ]]; then
-    echo "jdk not installed"
-else
-    echo "jdk successful installed"
-fi
-
-##安装tomcat
-[ -f /tmp/apache-tomcat-8.5.42.tar.gz ] && tar -xf /tmp/apache-tomcat-8.5.42.tar.gz -C /usr/local
-java -version
-retval=$?
-if [[ $retval -eq 0 ]]; then
-    /usr/bin/sh /usr/local/apache-tomcat-8.5.42/bin/startup.sh &>/dev/null
-    
-    if ps -elf|grep "tomcat"|egrep -v "grep" &>/dev/null; then
-        echo "tomcat successful installed"
-    else
-        echo "tomcat not installed,please jdk or other"
-    fi
-else
-    echo "check jdk"
-fi
 
 ##安装mysql
 [ -f /tmp/mysql-5.7.26-el7-x86_64.tar.gz ] && tar -xf /tmp/mysql-5.7.26-el7-x86_64.tar.gz -C /usr/local
@@ -64,7 +27,7 @@ ldconfig
 echo "PATH=$PATH:/usr/local/mysql/bin" > /etc/profile.d/mysql.sh
 source /etc/profile.d/mysql.sh
 chkconfig mysqld on
-cat > /etc/my.cnf <<-EOF
+cat > /etc/my.cnf <<-"EOF"
 [mysqld]
 basedir=/usr/local/mysql #mysql路径
 datadir=/data/mysqldata #mysql数据目录
@@ -83,12 +46,14 @@ fi
 
 old_passwd=$(sed -n '$'p /tmp/my_passwd.txt |awk -F': ' '{print $NF}')
 new_passwd="123456"
-/usr/local/mysql/bin/mysql -uroot -p{$old_passwd} <<-EOF
-set password='123456';
+/usr/local/mysql/bin/mysql -uroot -p"${old_passwd}" -b --connect-expired-password <<-EOF
+set password for 'root'@'localhost'=password("$new_passwd");
 flush privileges;
 EOF
-/usr/local/mysql/bin/mysql -uroot -p${new_passwd} -e "show databases;" &> /dev/null
-retval=$?
+
+#mysql -uroot '-pkUeAt=u#L2*Z' -e "set password for 'root'@'localhost'=password('123456');" -b --connect-expired-password
+
+/usr/local/mysql/bin/mysql -uroot -p"${new_passwd}" -e "show databases;"
 if [[ $retval -eq 0 ]]; then
     echo "mysql successful installed"
 else
@@ -103,77 +68,115 @@ cd /tmp/nginx-1.12.2
 ./configure --prefix=/usr/local/nginx --with-http_ssl_module --user=nginx  --group=nginx && make && make install
 
 ##nginx启动脚本
-cat >/etc/init.d/nginx<<-EOF
-#!/bin/bash  
- 
-nginx=/usr/local/nginx/sbin/nginx  
-nginx_config=/usr/local/nginx/conf/nginx.conf  
-RETVAL=0  
-prog="nginx"  
+cat >/etc/init.d/nginx<<-"EOF"
+#!/bin/sh
+#
+# nginx - this script starts and stops the nginx daemon
+#
+# chkconfig:   - 85 15
+# description: Nginx is an HTTP(S) server, HTTP(S) reverse \
+#               proxy and IMAP/POP3 proxy server
+# processname: nginx
+# config:      /etc/nginx/nginx.conf
+# config:      /etc/sysconfig/nginx
+# pidfile:     /var/run/nginx.pid
 
-# Source function library.  
-. /etc/rc.d/init.d/functions  
-# Source networking configuration.  
-. /etc/sysconfig/network  
-# Check that networking is up.  
-[ ${NETWORKING} = "no" ] && exit 0  
-[ -x $nginx ] || exit 0  
-# Start nginx daemons functions.  
-start() {  
-if [ -e $nginx_pid ];then  
-   echo "nginx already running...."  
-   exit 1  
-fi  
-   echo -n $"Starting $prog: "  
-   daemon $nginx -c ${nginx_config}  
-   RETVAL=$?  
-   echo  
-   [ $RETVAL = 0 ] && touch /var/lock/subsys/nginx  
-   return $RETVAL  
-}  
-# Stop nginx daemons functions.  
-stop() {  
-        echo -n $"Stopping $prog: "  
-        killproc $nginx  
-        RETVAL=$?  
-        echo  
-        [ $RETVAL = 0 ] && rm -f /var/lock/subsys/nginx /var/run/nginx.pid  
-}  
-# reload nginx service functions.  
-reload() {  
-    echo -n $"Reloading $prog: "   
-    killproc $nginx -HUP  
-    RETVAL=$?  
-    echo  
-}  
-# See how we were called.  
-case "$1" in  
-start)  
-        start  
-        ;;  
-stop)  
-        stop  
-        ;;  
-reload)  
-        reload  
-        ;;  
-restart)  
-        stop  
-        start  
-        ;;  
-status)  
-        status $prog  
-        RETVAL=$?  
-        ;;  
-*)  
-        echo $"Usage: $prog {start|stop|restart|reload|status|help}"  
-        exit 1  
-esac  
-exit $RETVAL
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+# Source networking configuration.
+. /etc/sysconfig/network
+
+# Check that networking is up.
+[ "$NETWORKING" = "no" ] && exit 0
+
+nginx="/usr/local/nginx/sbin/nginx"
+prog=$(basename $nginx)
+
+NGINX_CONF_FILE="/usr/local/nginx/conf/nginx.conf"
+
+[ -f /etc/sysconfig/nginx ] && . /etc/sysconfig/nginx
+
+lockfile=/var/lock/subsys/nginx
+
+start() {
+    [ -x $nginx ] || exit 5
+    [ -f $NGINX_CONF_FILE ] || exit 6
+    echo -n $"Starting $prog: "
+    daemon $nginx -c $NGINX_CONF_FILE
+    retval=$?
+    echo
+    [ $retval -eq 0 ] && touch $lockfile
+    return $retval
+}
+stop() {
+    echo -n $"Stopping $prog: "
+    killproc $prog -QUIT
+    retval=$?
+    echo
+    [ $retval -eq 0 ] && rm -f $lockfile
+    return $retval
+killall -9 nginx
+}
+restart() {
+    configtest || return $?
+    stop
+    sleep 1
+    start
+}
+reload() {
+    configtest || return $?
+    echo -n $"Reloading $prog: "
+    killproc $nginx -HUP
+RETVAL=$?
+    echo
+}
+force_reload() {
+    restart
+}
+configtest() {
+$nginx -t -c $NGINX_CONF_FILE
+}
+rh_status() {
+    status $prog
+}
+rh_status_q() {
+    rh_status >/dev/null 2>&1
+}
+case "$1" in
+    start)
+        rh_status_q && exit 0
+    $1
+        ;;
+    stop)
+        rh_status_q || exit 0
+        $1
+        ;;
+    restart|configtest)
+        $1
+        ;;
+    reload)
+        rh_status_q || exit 7
+        $1
+        ;;
+    force-reload)
+        force_reload
+        ;;
+    status)
+        rh_status
+        ;;
+    condrestart|try-restart)
+        rh_status_q || exit 0
+            ;;
+    *)    
+      echo $"Usage: $0 {start|stop|status|restart|condrestart|try-restart|reload|force-reload|configtest}"
+        exit 2
+esac
 EOF
+chmod +x /etc/init.d/nginx
 
 ##nginx配置文件
-cat > /usr/local/nginx/conf/nginx.conf <<-EOF
+cat > /usr/local/nginx/conf/nginx.conf <<-"EOF"
 # For more information on configuration, see:# * Official English Documentation: http://nginx.org/en/docs/# * Official Russian Documentation: http://nginx.org/ru/docs/
 
 user nginx;
@@ -242,54 +245,59 @@ chkconfig nginx on
 ##安装php
 useradd -s /sbin/nologin php-fpm
 yum install libxml2-devel curl-devel libjpeg-turbo-devel libpng-devel freetype-devel -y
-[ -f /tmp/php-7.2.21.tar.gz ] && tar -xf /tmp/php-7.2.21.tar.gz
-cd /tmp/php-7.2.21
-./configure --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc --enable-fpm --with-fpm-user=php-fpm --with-fpm-group=php-fpm \
---with-mysqli --with-mysql-sock=/tmp/mysql.sock --with-libxml-dir --with-gd --with-jpeg-dir --with-png-dir --with-freetype-dir --with-iconv-dir \
---with-zlib-dir --with-mcrypt --enable-soap --enable-ftp --enable-mbstring --enable-exif --disable-ipv6 \
---with-pear --with-curl --with-openssl --with-gettext --enable-bcmath --enable-sockets &>/dev/null && make &>/dev/null && make install
-cp /tmp/php-7.2.21/php.ini-production /usr/local/php/etc/php.ini
-mv /usr/local/php/etc/php-fpm.conf.default /usr/local/php/etc/php-fpm.conf
-mv /usr/local/php/etc/php-fpm.d/www.conf.default /usr/local/php/etc/php-fpm.d/www.conf
-/usr/local/php/sbin/php-fpm -t
-if [[ $? -eq 0 ]]; then
-    echo "php-fpm installed successful"
+[ -f /tmp/php-7.2.21.tar.gz ] && tar -xf /tmp/php-7.2.21.tar.gz -C /tmp
+if [ -d /tmp/php-7.2.21 ] ;then
+    cd /tmp/php-7.2.21
+    ./configure --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc --enable-fpm --with-fpm-user=php-fpm --with-fpm-group=php-fpm \
+    --with-mysqli --with-mysql-sock=/tmp/mysql.sock --with-libxml-dir --with-gd --with-jpeg-dir --with-png-dir --with-freetype-dir --with-iconv-dir \
+    --with-zlib-dir --with-mcrypt --enable-soap --enable-ftp --enable-mbstring --enable-exif --disable-ipv6 \
+    --with-pear --with-curl --with-openssl --with-gettext --enable-bcmath --enable-sockets &>/dev/null && make &>/dev/null && make install
+    cp /tmp/php-7.2.21/php.ini-production /usr/local/php/etc/php.ini
+    mv /usr/local/php/etc/php-fpm.conf.default /usr/local/php/etc/php-fpm.conf
+    mv /usr/local/php/etc/php-fpm.d/www.conf.default /usr/local/php/etc/php-fpm.d/www.conf
+    /usr/local/php/sbin/php-fpm -t
+    if [[ $? -eq 0 ]]; then
+        echo "php-fpm installed successful"
+    else
+        echo "php-fpm not installed"
+    fi
+    cp /tmp/php-7.2.21/sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
+    chmod 755 /etc/init.d/php-fpm
+    service php-fpm start
+
+    if ps -elf|grep "php-fpm"|egrep -v "grep" &>/dev/null; then
+        echo "php-fpm successful installed"
+    else
+        echo "php-fpm not installed"
+    fi
+    chkconfig php-fpm on
+
+    cat >/etc/profile.d/php.sh<<-"EOF"
+    export PATH=$PATH:/usr/local/php/bin
+    EOF
+    source /etc/profile.d/php.sh
+
+    php -v &>/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "php successful installed"
+    else
+        echo "php not installed,please check"
+    fi
+
+    ##nginx+php for test
+    cat >/usr/local/nginx/html/test.php<<-EOF
+    <?php
+    phpinfo();
+    ?>
+    EOF
+
+    curl -I http://127.0.0.1/test.php
+    if [[ $? -eq 0 ]]; then
+        echo "php+nginx successful installed"
+    else
+        echo "php+nginx not installed,please check"
+    fi
 else
-    echo "php-fpm not installed"
-fi
-cp /tmp/php-7.2.21/sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
-chmod 755 /etc/init.d/php-fpm
-service php-fpm start
-
-if ps -elf|grep "php-fpm"|egrep -v "grep" &>/dev/null; then
-    echo "php-fpm successful installed"
-else
-    echo "php-fpm not installed"
-fi
-chkconfig php-fpm on
-
-cat >/etc/profile.d/php.sh<<-EOF
-export PATH=$PATH:/usr/local/php/bin
-EOF
-source /etc/profile.d/php.sh
-
-php -v &>/dev/null
-if [[ $? -eq 0 ]]; then
-    echo "php successful installed"
-else
-    echo "php not installed,please check"
-fi
-
-##test
-cat >/usr/local/nginx/html/test.php<<-EOF
-<?php
-phpinfo();
-?>
-EOF
-
-curl -I http://127.0.0.1/test.php
-if [[ $? -eq 0 ]]; then
-    echo "php+nginx successful installed"
-else
-    echo "php+nginx not installed,please check"
+    echo "check /tmp/php-7.2.21"
+    exit 2
 fi
